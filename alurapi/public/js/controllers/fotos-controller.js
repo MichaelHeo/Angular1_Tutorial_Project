595cbc30b252c02ba735c6bef74a1ef429d358f8
Michael Heo <michael.kr95@gmail.com>
2017-07-04 00:44:40 -0300

    Class 3 - ng-repeat, $http

--- alurapi/public/js/controllers/fotos-controller.js
+++ alurapi/public/js/controllers/fotos-controller.js
@@ -1,9 +1,22 @@
-angular.module('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope, $http){
     // $scope Cria um escopo desse controller
-    
-    $scope.foto = {
-        titulo: 'Leao',
-        url: 'https://i.ytimg.com/vi/92r4CSt8txw/maxresdefault.jpg'
-    }; // Adicionou a foto dinamicamente no escopo do controller
+     
+    $scope.fotos = []; // Lista de fotos!
+
+    var promise = $http.get('v1/fotos'); // $http faz uma "promesa". Como se ele disesse que vai tentar pegar as fotos do servidor
+    promise.then(function(retorno){
+        $scope.fotos = retorno.data;
+    }).catch(function(error){ // Se a promessa for cumprida, dai vai ser armazenado no fotos
+        console.log("error");  
+    }); 
+    /* ---------------- O codigo abaixo faz a mesma! ----------------
+     * $http.get('v1/fotos')
+     * .success(function(fotos){
+     *      $scope.fotos = fotos;
+     * })
+     * .error(function(erro) {
+     *      console.log(erro);
+     * }
+     */
 
 });
