7c63449afa03833ace353f7d60854537c58915d0
Michael Heo <michael.kr95@gmail.com>
2017-07-04 01:32:08 -0300

    Class 4 - ddo, novas diretivas, marcacao e definicao

--- alurapi/public/js/controllers/fotos-controller.js
+++ alurapi/public/js/controllers/fotos-controller.js
@@ -3,20 +3,12 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
      
     $scope.fotos = []; // Lista de fotos!
 
-    var promise = $http.get('v1/fotos'); // $http faz uma "promesa". Como se ele disesse que vai tentar pegar as fotos do servidor
-    promise.then(function(retorno){
-        $scope.fotos = retorno.data;
-    }).catch(function(error){ // Se a promessa for cumprida, dai vai ser armazenado no fotos
-        console.log("error");  
-    }); 
-    /* ---------------- O codigo abaixo faz a mesma! ----------------
-     * $http.get('v1/fotos')
-     * .success(function(fotos){
-     *      $scope.fotos = fotos;
-     * })
-     * .error(function(erro) {
-     *      console.log(erro);
-     * }
-     */
+    $http.get('v1/fotos')
+    .success(function(fotos){
+         $scope.fotos = fotos;
+    })
+    .error(function(erro) {
+         console.log(erro);
+    });
 
 });
--- alurapi/public/js/main.js
+++ alurapi/public/js/main.js
@@ -1,2 +1,2 @@
-angular.module('alurapic', []); // Criando um modulo do Angular
+angular.module('alurapic', ['minhasDiretivas']); // Criando um modulo do Angular
 // So se coloca o segundo parametro "[]" pq esse eh um modulo sendo criado! Se fosse para utilizar um modulo, ficaria sem o "[]"
