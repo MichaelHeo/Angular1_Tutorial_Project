d4713b41a608418ffd5ece0717adaeb8a346b3db
Michael Heo <michael.kr95@gmail.com>
2017-07-05 01:28:34 -0300

    Class 5 - ng-model, ng-repeat filtro, ng-model-options, animacao com ngAnimate, animacao CSS

--- alurapi/public/js/controllers/fotos-controller.js
+++ alurapi/public/js/controllers/fotos-controller.js
@@ -2,6 +2,7 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
     // $scope Cria um escopo desse controller
      
     $scope.fotos = []; // Lista de fotos!
+    $scope.filtro = '';
 
     $http.get('v1/fotos')
     .success(function(fotos){
